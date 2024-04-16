701e03b9817e428d88457ed8bf54a9a8bbe58bed
Captain Lucian <~Me@Don'tShowMyEmail.com>
2024-04-16 13:45:13 -0500

    First version that functions and adds the shortcut. Still plenty of cleanup to do I'm sure

--- InfiniteFusion.sh
+++ InfiniteFusion.sh
@@ -6,7 +6,7 @@ set -o pipefail     # capture error from pipes
 # $USER
 [[ -n $(logname >/dev/null 2>&1) ]] && logged_in_user=$(logname) || logged_in_user=$(whoami)
 
-echo "Downloading Pokemon Infinite Fusion 6.1.3 to home/user/Games/Testing Ground"
+echo "Downloading/Updating Pokemon Infinite Fusion at home/user/Games/Testing Ground"
 if [ -d ~/Games/"Testing Ground" ]
 then
     echo "Folder exists, moving on"
@@ -35,3 +35,169 @@ git remote add origin "https://github.com/infinitefusion/infinitefusion-e18.git"
 git fetch origin releases
 git reset --hard origin/releases
 echo "Install complete, adding to Steam.."
+
+#Buckle up,things are getting a lot more technical and a lot more copy/paste from here on in.
+
+
+
+#logged_in_home="~" # I think you can get away without pulling actively logged in user but this variable is used so much I just want to leave it for now
+#Well it disagrees, let me try assuming basic ass user
+logged_in_home="/home/deck"
+# Set the default Steam directory
+steam_dir="${logged_in_home}/.local/share/Steam"
+echo $steam_dir
+
+# Check if the loginusers.vdf file exists
+if [[ -f "${logged_in_home}/.steam/root/config/loginusers.vdf" ]]; then
+    # Extract the block of text for the most recent user
+    most_recent_user=$(sed -n '/"users"/,/"MostRecent" "1"/p' "${logged_in_home}/.steam/root/config/loginusers.vdf")
+
+    # Initialize variables
+    max_timestamp=0
+    current_user=""
+    current_steamid=""
+
+    # Process each user block
+	# Set IFS to only look for Commas to avoid issues with Whitespace in older account names.
+    while IFS="," read steamid account timestamp; do
+        if (( timestamp > max_timestamp )); then
+            max_timestamp=$timestamp
+            current_user=$account
+            current_steamid=$steamid
+        fi
+	# Output our discovered values as comma seperated string to be read into the IDs.
+    done < <(echo "$most_recent_user" | awk -v RS='}\n' -F'\n' '
+    {
+        for(i=1;i<=NF;i++){
+            if($i ~ /[0-9]{17}/){
+                split($i,a, "\""); steamid=a[2];
+            }
+            if($i ~ /"AccountName"/){
+                split($i,b, "\""); account=b[4];
+            }
+            if($i ~ /"Timestamp"/){
+                split($i,c, "\""); timestamp=c[4];
+            }
+        }
+        print steamid "," account "," timestamp
+    }')
+
+    # Print the currently logged in user
+    if [[ -n $current_user ]]; then
+        echo "Currently logged in user: $current_user"
+        echo "SteamID: $current_steamid"
+    else
+        echo "No users found."
+    fi
+
+    # Convert steamid to steamid3
+    steamid3=$((current_steamid - 76561197960265728))
+
+    # Directly map steamid3 to userdata folder
+    userdata_folder="${logged_in_home}/.steam/root/userdata/${steamid3}"
+
+    # Check if userdata_folder exists
+    if [[ -d "$userdata_folder" ]]; then
+        echo "Found userdata folder for user with SteamID $current_steamid: $userdata_folder"
+    else
+        echo "Could not find userdata folder for user with SteamID $current_steamid"
+    fi
+else
+    echo "Could not find loginusers.vdf file"
+fi
+# Check if userdata folder was found
+if [[ -n "$userdata_folder" ]]; then
+    # Userdata folder was found
+    echo "Current user's userdata folder found at: $userdata_folder"
+
+    # Find shortcuts.vdf file for current user
+    shortcuts_vdf_path=$(find "$userdata_folder" -type f -name shortcuts.vdf)
+
+    # Check if shortcuts_vdf_path is not empty
+    if [[ -n "$shortcuts_vdf_path" ]]; then
+        # Create backup of shortcuts.vdf file
+        cp "$shortcuts_vdf_path" "$shortcuts_vdf_path.bak"
+    else
+        # Find config directory for current user
+        config_dir=$(find "$userdata_folder" -maxdepth 1 -type d -name config)
+
+        # Check if config_dir is not empty
+        if [[ -n "$config_dir" ]]; then
+            # Create new shortcuts.vdf file at expected location for current user
+            touch "$config_dir/shortcuts.vdf"
+            shortcuts_vdf_path="$config_dir/shortcuts.vdf"
+        else
+            # Create new config directory and new shortcuts.vdf file at expected location for current user
+            mkdir "$userdata_folder/config/"
+            touch "$userdata_folder/config/shortcuts.vdf"
+            config_dir="$userdata_folder/config/"
+            shortcuts_vdf_path="$config_dir/shortcuts.vdf"
+        fi
+    fi
+else
+    # Userdata folder was not found
+    echo "Current user's userdata folder not found"
+fi
+# Check if userdata folder was found
+if [[ -n "$userdata_folder" ]]; then
+    # Userdata folder was found
+    echo "Current user's userdata folder found at: $userdata_folder"
+
+    # Find shortcuts.vdf file for current user
+    shortcuts_vdf_path=$(find "$userdata_folder" -type f -name shortcuts.vdf)
+
+    # Check if shortcuts_vdf_path is not empty
+    if [[ -n "$shortcuts_vdf_path" ]]; then
+        # Create backup of shortcuts.vdf file
+        cp "$shortcuts_vdf_path" "$shortcuts_vdf_path.bak"
+    else
+        # Find config directory for current user
+        config_dir=$(find "$userdata_folder" -maxdepth 1 -type d -name config)
+
+        # Check if config_dir is not empty
+        if [[ -n "$config_dir" ]]; then
+            # Create new shortcuts.vdf file at expected location for current user
+            touch "$config_dir/shortcuts.vdf"
+            shortcuts_vdf_path="$config_dir/shortcuts.vdf"
+        else
+            # Create new config directory and new shortcuts.vdf file at expected location for current user
+            mkdir "$userdata_folder/config/"os.environ.ge
+            touch "$userdata_folder/config/shortcuts.vdf"
+            config_dir="$userdata_folder/config/"
+            shortcuts_vdf_path="$config_dir/shortcuts.vdf"
+        fi
+    fi
+else
+    # Userdata folder was not found
+    echo "Current user's userdata folder not found"
+fi
+
+
+# Pre check for updating the config file
+
+# Set the default Steam directory
+steam_dir_root="${logged_in_home}/.steam/root"
+
+# Set the path to the config.vdf file
+config_vdf_path="${steam_dir_root}/config/config.vdf"
+
+# Check if the config.vdf file exists
+if [ -f "$config_vdf_path" ]; then
+    # Create a backup of the config.vdf file
+    backup_path="${steam_dir_root}/config/config.vdf.bak"
+    cp "$config_vdf_path" "$backup_path"
+
+    # Set the name of the compatibility tool to use
+    compat_tool_name=$(ls "${logged_in_home}/.steam/root/compatibilitytools.d" | grep "GE-Proton" | sort -V | tail -n1)
+else
+    echo "Could not find config.vdf file"
+fi
+echo "export logged_in_home=$logged_in_home" >> ${logged_in_home}/.config/systemd/user/env_vars
+echo "export steamid3=$steamid3" >> ${logged_in_home}/.config/systemd/user/env_vars
+
+echo "installing python script dependency"
+python3 -m pip install vdf
+echo "starting python script"
+python3 ${logged_in_home}/Downloads/'Steam Shortcut Maker.py'
+
+echo "Shortcut has been added to Steam, it may need to be restarted to take effect"
---
+++ Steam Shortcut Maker.py
@@ -0,0 +1,75 @@
+#We need to import the library made to work with Valve's vdf format
+print('Python started')
+import vdf
+#importing libraries need to pull in variables
+import os, re
+
+# This code block is copied from NonSteamLaunchers-On-Steam-Deck to pull in the variables left by the Bash script
+# Path to the env_vars file
+env_vars_path = f"{os.environ['HOME']}/.config/systemd/user/env_vars"
+
+# Check if the env_vars file exists
+if not os.path.exists(env_vars_path):
+    print(f"Error: {env_vars_path} does not exist.")
+    sys.exit(1)
+
+# Read variables from the file
+with open(env_vars_path, 'r') as f:
+    lines = f.readlines()
+
+for line in lines:
+    if line.startswith('export '):
+        line = line[7:]  # Remove 'export '
+    name, value = line.strip().split('=', 1)
+    os.environ[name] = value
+
+# Variables from NonSteamLaunchers.sh
+logged_in_home = os.environ['logged_in_home']
+steamid3 = os.environ['steamid3']
+filepath = logged_in_home +'/.steam/steam/userdata/'+steamid3+'/config/shortcuts.vdf'
+print (filepath)
+#Binary Read in of the contents and stored as the variable "shortcuts"
+with open(filepath,'rb') as file:
+    shortcuts = vdf.binary_loads(file.read())
+#with open('shortcuts.vdf','rb') as file:
+
+#To easily inject the entry I'm adding it to a variable
+#A great place to improve this would be making the AppID auto generate to a unique value but that was quickly getting over my head
+#Another place to improve would be pulling in the home location variables for this to work for non-default users    
+startdir = '"'+logged_in_home+'/Games/InfiniteFusion/'+'"'
+exe = '"'+logged_in_home+'/Games/InfiniteFusion/Game.exe'+'"'
+print (exe)
+print (startdir)
+PIF = {
+    'appid': "-207231401",
+    'appname': "Testing PIF",
+    'Exe': exe,
+    'StartDir' : "/home/deck/Games/InfiniteFusion/",
+    'icon' : "",
+    'ShortcutPath' : "",
+    'LaunchOptions' : "",
+    'IsHidden': "0",
+    'AllowDesktopConfig': "1",
+    'AllowOverlay' : "1",
+    'OpenVR' : "0",
+    'Devkit' : "0",
+    'DevkitGameID' : "",
+    'DevkitOverideAppID' : "",
+    'LastPlayTime' : "",
+    'FlatpakAppID' : "",
+    'tags' : ""
+}
+#One of the complications I had was figuring out how to set the next entry number
+#The way this data comes in is flat and the only tier I can pull out is a whole entry, so I can't manipulate the number easily
+#I'm working around that by counting the number of entries in the dict and using that
+#Since the dict starts at 0, it works out perfect that the number of current entries will be the next number used
+
+NextID = (len(shortcuts))
+NextID = str(NextID)
+#Adding the entry to the dict using the entry number we made and defining that entry as the variable we set
+shortcuts['shortcuts'][NextID] = PIF
+#Finally we are writing the list we just made to the file
+with open(filepath,'wb') as file:
+    file.write(vdf.binary_dumps(shortcuts))
+#Dumps results to a text file so you can review the output. Uncomment it out if you are seeing issues.
+#vdf.dump(shortcuts, open('file2.txt','w'), pretty=True)
